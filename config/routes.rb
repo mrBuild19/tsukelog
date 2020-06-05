69177df68aaa3f32af3d0148685f188c29c4c9ce
mrBuild19 <rabits.tank@gmail.com>
2020-06-05 23:06:35 +0000

    #3 usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                     	sessions: 'users/sessions' }
+  resources :users, only: [:show, :edit]
 end
