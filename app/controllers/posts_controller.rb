f7bf86c0c65764e587103984d4e63de5c3b3475e
mrBuild19 <rabits.tank@gmail.com>
2020-06-06 09:04:39 +0000

    #5 投稿一覧画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def create
   	post = Post.new(post_params)
+  	post.user_id = current_user.id
   	post.save
   	redirect_to posts_path, notice: "登録しました。"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_080546) do
+ActiveRecord::Schema.define(version: 2020_06_06_083100) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
