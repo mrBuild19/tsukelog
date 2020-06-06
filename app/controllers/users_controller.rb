c321bb8cf03c526747f9b7ccce658b898452150e
mrBuild19 <rabits.tank@gmail.com>
2020-06-06 00:50:13 +0000

    #3 写真投稿用gemを導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:introduction, :profile_image)
   end
 end
