17a90cd95ef77f495959738f404d25d59515604e
mrBuild19 <rabits.tank@gmail.com>
2020-06-06 03:45:26 +0000

    #3 マイページ & プロフィール編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to user_path(@user)
+		else
+			render :edit
+		end
   end
 
   private
   def user_params
-    params.require(:user).permit(:introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e778df268d85b105675f18ad3650307213df3b3971555ab5349673dc7aa843a03223d53f56cfd56bf9e78c9cd87c773b9a00cb2df2940b9767444e28830adbca'
