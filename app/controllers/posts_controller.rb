9f143198ac4d27053fbc8071e330d7dd06ee25ab
mrBuild19 <rabits.tank@gmail.com>
2020-06-06 09:45:26 +0000

    #5 投稿編集画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	post = Post.find(params[:id])
+  	post.update(post_params)
+  	redirect_to post_path(post), notice: "投稿を更新しました。"
+  end
+
+  def destroy
+  	post = Post.find(params[:id])
+  	post.destroy
+  	redirect_to posts_path, notice: "投稿を削除しました。"
   end
 
   private
