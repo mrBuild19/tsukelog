24b75c5fe3a7d5798397b041a5854c5aaa4f0144
mrBuild19 <rabits.tank@gmail.com>
2020-06-07 04:05:08 +0000

    #16 postモデルの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ApplicationRecord
 	accepts_attachments_for :post_images, attachment: :post_image
 
 	def liked_by?(user)
-		likes.where(user_id: user.id).exsits?
+		likes.where(user_id: user.id).exists?
 	end
 end
