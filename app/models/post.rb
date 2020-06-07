417ed09608587064456930a83cb3d3fe92bdd280
mrBuild19 <rabits.tank@gmail.com>
2020-06-07 01:04:01 +0000

    #6 PostCommentモデル作成 & 各モデルに関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	has_many :post_images, dependent: :destroy
+	has_many :post_comments, dependent: :destroy
 	accepts_attachments_for :post_images, attachment: :post_image
 end
