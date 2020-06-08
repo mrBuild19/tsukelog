6dc3c142b1ceaa912b1c50f6f384ba64bff4dce1
mrBuild19 <rabits.tank@gmail.com>
2020-06-08 09:54:55 +0000

    #22 各モデルに関連付けを行う

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                     	sessions: 'users/sessions' }
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+  	member do
+  		get :following, :followers
+  	end
+  end
   resources :posts do
   	resources :post_comments, only: [:create, :destroy]
   	resource :likes, only: [:create, :destroy]
