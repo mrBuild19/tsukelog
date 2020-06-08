d41201d508c8e1385c67048099caf85b55452871
mrBuild19 <rabits.tank@gmail.com>
2020-06-08 14:16:10 +0000

    #22 フォロー、フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,29 +6,29 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship",
+  has_many :follower, class_name: "Relationship",
   					foreign_key: "follower_id",
   					dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship",
+  has_many :followed, class_name: "Relationship",
   					foreign_key: "followed_id",
   					dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :following_user, through: :follower, source: :followed
+  has_many :followers_user, through: :followed, source: :follower
   attachment :profile_image
 
 
   # ユーザーをフォローする
-  def follow(other_user)
-  	following << other_user
+  def follow(user_id)
+  	follower.create(followed_id: user_id)
   end
 
   # ユーザーをフォロー解除する
-  def unfollow(other_user)
-  	active_relationships.find_by(followed_id: other_user.id).destroy
+  def unfollow(user_id)
+  	follower.find_by(followed_id: user_id).destroy
   end
 
   # 現在のユーザーがフォローしてたらtrueを返す
-  def following?(other_user)
-  	following.include?(other_user)
+  def following?(user)
+  	following_user.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                     	sessions: 'users/sessions' }
-  resources :users, only: [:show, :edit, :update] do
-  	member do
-  		get :following, :followers
-  	end
-  end
+  resources :users, only: [:show, :edit, :update]
   resources :posts do
   	resources :post_comments, only: [:create, :destroy]
   	resource :likes, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow' #フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' #フォロー外す
 end
