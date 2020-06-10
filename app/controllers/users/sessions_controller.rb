15b1fc50b6a66e6dba4f60a07eb4fc280c159e20
mrBuild19 <rabits.tank@gmail.com>
2020-06-10 14:01:43 +0000

    #29 ログイン後のリンク先を調整

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,14 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  #ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
+  #ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
