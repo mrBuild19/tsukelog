419b358787457b3fa7a80e496987521f9cd026d7
mrBuild19 <rabits.tank@gmail.com>
2020-06-12 06:32:44 +0000

    #34 コメント機能を非同期通信

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,17 +1,18 @@
 class PostCommentsController < ApplicationController
 	def create
-		post = Post.find(params[:post_id])
+		@post = Post.find(params[:post_id])
 		comment = PostComment.new(post_comment_params)
 		comment.user_id = current_user.id
-		comment.post_id = post.id
+		comment.post_id = @post.id
 		comment.save
-		redirect_to post_path(post)
+		render :index
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
-		@comment = PostComment.find_by(id: params[:id], post_id: @post.id)
-		@comment.destroy
+		comment = PostComment.find_by(id: params[:id], post_id: @post.id)
+		comment.destroy
+		render :index
 	end
 
 	private
