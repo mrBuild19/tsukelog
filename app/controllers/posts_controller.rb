0936f5c996f24e3ad127df35eb1aac2b09a384d4
mrBuild19 <rabits.tank@gmail.com>
2020-06-20 09:46:19 +0000

    #62 新規投稿画面に実装(編集可能)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,36 +11,36 @@ class PostsController < ApplicationController
   end
 
   def new
-  	@post = Post.new
-  	@post.post_images.build
+    @post = Post.new
+    @post.post_images.build
   end
 
   def create
-  	post = Post.new(post_params)
-  	post.user_id = current_user.id
-  	post.save
-  	redirect_to posts_path, notice: "登録しました。"
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    post.save
+    redirect_to posts_path, notice: "登録しました。"
   end
 
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     @comment = PostComment.new
   end
 
   def edit
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def update
-  	post = Post.find(params[:id])
-  	post.update(post_params)
-  	redirect_to post_path(post), notice: "投稿を更新しました。"
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post), notice: "投稿を更新しました。"
   end
 
   def destroy
-  	post = Post.find(params[:id])
-  	post.destroy
-  	redirect_to posts_path, notice: "投稿を削除しました。"
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました。"
   end
 
   def search
@@ -55,6 +55,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :text, :tag_list, post_images_post_images: [])
+    params.require(:post).permit(:title, :text, :rate, :tag_list, post_images_post_images: [])
   end
 end
