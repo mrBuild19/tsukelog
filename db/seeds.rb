e25e72833f25c92e1465bff375a569b5e00df0c7
mrBuild19 <rabits.tank@gmail.com>
2020-06-20 14:42:57 +0000

    #63 投稿詳細画面(投稿・コメント削除)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "admin",
+             email: "admin@gmail.com",
+             password:  "mastercode",
+             password_confirmation: "mastercode",
+             admin: true)
