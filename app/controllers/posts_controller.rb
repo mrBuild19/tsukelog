0cd49989558fa0dba514f4823b3490448af949b5
mrBuild19 <rabits.tank@gmail.com>
2020-06-22 07:44:43 +0000

    #71 postsコントローラーの条件分岐

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,10 +16,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.user_id = current_user.id
-    post.save
-    redirect_to posts_path, notice: "登録しました。"
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました。"
+    else
+      render :new
+    end
   end
 
   def show
@@ -32,15 +35,18 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post), notice: "投稿を更新しました。"
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "投稿を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    flash[:notice] = "投稿を削除しました。"
+    flash[:alert] = "投稿を削除しました。"
     if params[:admin].present?
       redirect_back(fallback_location: posts_path)
     else
