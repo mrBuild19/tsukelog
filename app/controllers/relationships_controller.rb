c327d202fc2367e22b8b7b67bc50992ff78b3b8b
mrBuild19 <rabits.tank@gmail.com>
2020-06-23 02:53:17 +0000

    #83 管理者用のアラート文が誤表示を修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,7 +13,9 @@ class RelationshipsController < ApplicationController
 
 	private
 	def admin_limit
-		flash[:alert] = "管理者ユーザーはフォローできません。"
-    	redirect_back(fallback_location: posts_path) if current_user.admin?
+		if current_user.admin?
+			flash[:alert] = "管理者ユーザーはフォローできません。"
+    		redirect_back(fallback_location: posts_path)
+    	end
   	end
 end
