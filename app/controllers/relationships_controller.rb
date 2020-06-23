a42139f0c41c04e59cdcd1044a348c02a6bd849e
mrBuild19 <rabits.tank@gmail.com>
2020-06-23 01:50:11 +0000

    #80 管理者ユーザーでフォロー機能をできないよう修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,19 @@
 class RelationshipsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :admin_limit
+
 	def follow
 		current_user.follow(params[:id])
 		redirect_back(fallback_location: user_path(params[:id]))
 	end
-
 	def unfollow
 		current_user.unfollow(params[:id])
 		redirect_back(fallback_location: user_path(params[:id]))
 	end
+
+	private
+	def admin_limit
+		flash[:alert] = "管理者ユーザーはフォローできません。"
+    	redirect_back(fallback_location: posts_path) if current_user.admin?
+  	end
 end
