b836a9006c61028192a6827f23ee8650189e5c16
mrBuild19 <rabits.tank@gmail.com>
2020-08-04 16:19:06 +0900

    #105 コーディング違反箇所の修正/postモデル

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,17 @@
 class Post < ApplicationRecord
-	belongs_to :user
-	has_many :post_images, dependent: :destroy
-	has_many :post_comments, dependent: :destroy
-	has_many :likes, dependent: :destroy
-	accepts_attachments_for :post_images, attachment: :image
-	acts_as_taggable
+  belongs_to :user
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  accepts_attachments_for :post_images, attachment: :image
+  acts_as_taggable
 
-	validates :user_id, presence: true
-	validates :title, presence: true, length: { maximum: 20 }
-	validates :text, length: { maximum: 140 }
-	validates :rate, presence: true
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :text, length: { maximum: 140 }
+  validates :rate, presence: true
 
-
-	def liked_by?(user)
-		likes.where(user_id: user.id).exists?
-	end
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
