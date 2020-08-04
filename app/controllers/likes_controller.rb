1d2d43f77d23b409b6cedf7b7f23e171db4765b0
mrBuild19 <rabits.tank@gmail.com>
2020-08-04 16:16:10 +0900

    #105 コーディング違反箇所の修正/likeコントローラー

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,27 @@
 class LikesController < ApplicationController
-	before_action :authenticate_user!
-	before_action :set_post
-	before_action :admin_limit
+  before_action :authenticate_user!
+  before_action :set_post
+  before_action :admin_limit
 
-	def create
-		if current_user != @post.user
-			like = current_user.likes.new(post_id: @post.id)
-			like.save
-		end
-	end
-	def destroy
-		like = current_user.likes.find_by(post_id: @post.id)
-		like.destroy
-	end
+  def create
+    if current_user != @post.user
+      like = current_user.likes.new(post_id: @post.id)
+      like.save
+    end
+  end
 
-	private
-	def set_post
-		@post = Post.find(params[:post_id])
-	end
-	def admin_limit
-    	redirect_back(fallback_location: posts_path) if current_user.admin?
-  	end
+  def destroy
+    like = current_user.likes.find_by(post_id: @post.id)
+    like.destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def admin_limit
+    redirect_back(fallback_location: posts_path) if current_user.admin?
+  end
 end
