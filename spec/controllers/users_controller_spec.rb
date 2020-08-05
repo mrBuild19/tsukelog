56180050ab77470de4473f7af9e22d597b9af1ca
mrBuild19 <rabits.tank@gmail.com>
2020-08-05 15:24:22 +0900

    #95 userコントローラーのindex, show, searchアクションのテストを追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,110 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+	describe "#index" do
+
+		before do
+			@user = FactoryBot.create(:user)
+		end
+
+		# 正常にレスポンスを返すこと
+		it "responds successfully" do
+			get :index
+			expect(response).to be_success
+		end
+
+		# 200レスポンスを返すこと
+		it "returns a 200 response" do
+			get :index
+			expect(response).to have_http_status "200"
+		end
+	end
+
+	describe "#show" do
+
+		# 認可されたユーザーとして
+		context "as an authorized user" do
+
+			before do
+				@user = FactoryBot.create(:user)
+			end
+
+			# 正常にレスポンスを返すこと
+			it "responds successfully" do
+				sign_in @user
+				get :show, params: { id: @user.id }
+				expect(response).to be_success
+			end
+
+			# 200レスポンスを返すこと
+			it "returns a 200 response" do
+				sign_in @user
+				get :show, params: { id: @user.id }
+				expect(response).to have_http_status "200"
+			end
+		end
+
+		# 認可されていないユーザーとして
+		context "as an unauthorized user" do
+
+			before do
+				@user = FactoryBot.create(:user)
+				@other_user = FactoryBot.create(:user)
+			end
+
+			# 正常にレスポンスを返すこと
+			it "responds successfully" do
+				sign_in @user
+				get :show, params: { id: @other_user.id }
+				expect(response).to be_success
+			end
+
+			# 200レスポンスを返すこと
+			it "returns a 200 response" do
+				sign_in @user
+				get :show, params: { id: @other_user.id }
+				expect(response).to have_http_status "200"
+			end
+		end
+
+		# ログインしていないユーザーとして
+		context "as not logged in user" do
+
+			before do
+				@other_user = FactoryBot.create(:user)
+			end
+
+			# 302レスポンスを返すこと
+			it "returns a 302 response" do
+				get :show, params: { id: @other_user.id }
+				expect(response).to have_http_status "302"
+			end
+
+			# サインイン画面にリダイレクトすること
+			it "redirects to the sign-in page" do
+				get :show, params: { id: @other_user.id }
+				expect(response).to redirect_to "/users/sign_in"
+			end
+		end
+	end
+
+	describe "#search" do
+
+		before do
+			@user = FactoryBot.create(:user)
+		end
+
+		# 正常にレスポンスを返すこと
+		it "responds successfully" do
+			get :search
+			expect(response).to be_success
+		end
+
+		# 200レスポンスを返すこと
+		it "returns a 200 response" do
+			get :search
+			expect(response).to have_http_status "200"
+		end
+	end
+
 end
