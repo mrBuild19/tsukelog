68a3c5d2fc24d00c03db35280e630494197ed70a
mrBuild19 <rabits.tank@gmail.com>
2020-08-05 15:54:55 +0900

    #95 userコントローラーのeditアクションのテストを追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -89,6 +89,66 @@ RSpec.describe UsersController, type: :controller do
 		end
 	end
 
+	describe "#edit" do
+
+		# 認可されたユーザーとして
+		context "as an authorized user" do
+
+			before do
+				@user = FactoryBot.create(:user)
+			end
+
+			# 正常にレスポンスを返すこと
+			it "responds successfully" do
+				sign_in @user
+				get :edit, params: { id: @user.id }
+				expect(response).to be_success
+			end
+
+			# 200レスポンスを返すこと
+			it "returns a 200 response" do
+				sign_in @user
+				get :edit, params: { id: @user.id }
+				expect(response).to have_http_status "200"
+			end
+		end
+
+		# 管理者ユーザーとして
+		context "as an admin user" do
+
+			before do
+				@admin_user = FactoryBot.create(:user, admin: true)
+			end
+
+			# ユーザー一覧画面にリダイレクトすること
+			it "redirects to the user-index page" do
+				sign_in @admin_user
+				get :edit, params: { id: @admin_user.id }
+				expect(response).to redirect_to "/users"
+			end
+		end
+
+		# ログインしていないユーザーとして
+		context "as not logged in user" do
+
+			before do
+				@other_user = FactoryBot.create(:user)
+			end
+
+			# 302レスポンスを返すこと
+			it "returns a 302 response" do
+				get :edit, params: { id: @other_user.id }
+				expect(response).to have_http_status "302"
+			end
+
+			# サインイン画面にリダイレクトすること
+			it "redirects to the sign-in page" do
+				get :edit, params: { id: @other_user.id }
+				expect(response).to redirect_to "/users/sign_in"
+			end
+		end
+	end
+
 	describe "#search" do
 
 		before do
