2ed31ffa3d1ff9bd4fc6c50af4bd9703920c7288
mrBuild19 <rabits.tank@gmail.com>
2020-08-06 09:42:38 +0900

    #95 postコントローラーのnewアクションのテスト追加

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -24,6 +24,52 @@ RSpec.describe PostsController, type: :controller do
 		end
 	end
 
+	describe "#new" do
+
+		# ログインユーザーとして
+		context "as login user" do
+
+			before do
+				@post = FactoryBot.create(:post)
+				@user = @post.user
+			end
+
+			# 正常にレスポンスを返すこと
+			it "responds successfully" do
+				sign_in @user
+				get :new
+				expect(response).to be_success
+			end
+
+			# 200レスポンスを返すこと
+			it "returns a 200 response" do
+				sign_in @user
+				get :new
+				expect(response).to have_http_status "200"
+			end
+		end
+
+		# ログインしていないユーザーとして
+		context "as not logged in user" do
+
+			before do
+				@post = FactoryBot.create(:post)
+			end
+
+			# 302レスポンスを返すこと
+			it "returns a 302 response" do
+				get :new
+				expect(response).to have_http_status "302"
+			end
+
+			# サインイン画面にリダイレクトすること
+			it "redirects to the sign-in page" do
+				get :new
+				expect(response).to redirect_to "/users/sign_in"
+			end
+		end
+	end
+
 	describe "#show" do
 
 		# ログインユーザーとして
