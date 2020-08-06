ec4dc9f7ccbcbde46ebd24d92a402c56da3d782d
mrBuild19 <rabits.tank@gmail.com>
2020-08-06 09:44:44 +0900

    #95 postコントローラーのindexアクションにログインしていないユーザーのテスト追加

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,23 +4,47 @@ RSpec.describe PostsController, type: :controller do
 
 	describe "#index" do
 
-		before do
-			@post = FactoryBot.create(:post)
-			@user = @post.user
+		# ログインユーザーとして
+		context "as login user" do
+
+			before do
+				@post = FactoryBot.create(:post)
+				@user = @post.user
+			end
+
+			# 正常にレスポンスを返すこと
+			it "responds successfully" do
+				sign_in @user
+				get :index
+				expect(response).to be_success
+			end
+
+			# 200レスポンスを返すこと
+			it "returns a 200 response" do
+				sign_in @user
+				get :index
+				expect(response).to have_http_status "200"
+			end
 		end
 
-		# 正常にレスポンスを返すこと
-		it "responds successfully" do
-			sign_in @user
-			get :index
-			expect(response).to be_success
-		end
+		# ログインしていないユーザーとして
+		context "as not logged in user" do
 
-		# 200レスポンスを返すこと
-		it "returns a 200 response" do
-			sign_in @user
-			get :index
-			expect(response).to have_http_status "200"
+			before do
+				@post = FactoryBot.create(:post)
+			end
+
+			# 302レスポンスを返すこと
+			it "returns a 302 response" do
+				get :index
+				expect(response).to have_http_status "302"
+			end
+
+			# サインイン画面にリダイレクトすること
+			it "redirects to the sign-in page" do
+				get :index
+				expect(response).to redirect_to "/users/sign_in"
+			end
 		end
 	end
 
